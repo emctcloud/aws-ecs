e2afe5860cb9e1935b26640862acc686929e3e24
Emanuel Claudino <emanuelclaudinotx@gmail.com>
2024-09-12 23:05:31 -0300

    Feat: bastionHost

--- terraform/bastionHost.tf
+++ terraform/bastionHost.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "this" {
   name = "aws_sg_01"
-  description = ""
+  description = "Allows SSH Conection"
   vpc_id = aws_vpc.ecs-vpc.id
 
   ingress {
@@ -9,10 +9,18 @@ resource "aws_security_group" "this" {
     protocol = "tcp"
     cidr_blocks = [ var.allowed_ip ]
   }
-  egress = {
+  egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
+}
+
+resource "aws_instance" "this" {
+  ami = "ami-0182f373e66f89c85"
+  instance_type = var.instance_type
+  key_name = var.key_name
+  subnet_id = aws_subnet.ecs_sub_pub_1a.id
+  vpc_security_group_ids = [ aws_security_group.this.id ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,12 @@ variable "allowed_ip" {
   description = "IP/CIDR"
   type = string
 }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+  default = "t2.micro"
+}
+variable "key_name" {
+  description = "SSH Key Pair"
+  type = string
+}
